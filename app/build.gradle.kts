0b595c4ddc69ce25c3b1690e63e97046cbc47afa
CairraoP <aluno25942@ipt.pt>
2026-02-07 14:41:23 +0000

    Refactor package to spitifi- gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "pt.ipt.api"
+    namespace = "pt.ipt.spitifi"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "pt.ipt.api"
+        applicationId = "pt.ipt.spitifi"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
